33bd19808f08630e780c5c3dc3ed53d48887f1ab
Marek Odrzywolek <m.odrzywolek99@gmail.com>
2021-01-16 22:30:50 +0100

    Add possibility to add employee

--- src/api/EmployeesManagementRequests.js
+++ src/api/EmployeesManagementRequests.js
@@ -3,9 +3,24 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:8080/';
 
 export function getAllEmployees(jwtToken) {
-    return axios.get(`${baseUrl}admin/employees`,{ headers: { 'Authorization': "Bearer " + jwtToken } });
+    return axios.get(`${baseUrl}admin/employees`, {headers: {'Authorization': "Bearer " + jwtToken}});
 }
 
 export function deleteEmployee(employeeId, jwtToken) {
-    return axios.delete(`${baseUrl}admin/employees/` + employeeId,{ headers: { 'Authorization': "Bearer " + jwtToken } });
+    return axios.delete(`${baseUrl}admin/employees/` + employeeId, {headers: {'Authorization': "Bearer " + jwtToken}});
+}
+
+export function addEmployee(registerData, jwtToken) {
+    let registerEmployeeBody = {
+        "email": registerData.email,
+        "password": registerData.password,
+        "name": registerData.name,
+        "surname": registerData.surname,
+        "phoneNr": registerData.phoneNr,
+        "birthdate": registerData.birthDate,
+        "address": registerData.address,
+        "position": registerData.position,
+        "salary": registerData.salary
+    };
+    return axios.post(`${baseUrl}admin/employees/`, registerEmployeeBody, {headers: {'Authorization': "Bearer " + jwtToken}});
 }
