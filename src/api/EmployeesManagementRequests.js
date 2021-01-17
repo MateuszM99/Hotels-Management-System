ff0d5cd66a3bf055997d5c7a1ae5c895bd8338f9
Marek Odrzywolek <m.odrzywolek99@gmail.com>
2021-01-17 01:26:46 +0100

    Admin can create schedules

--- src/api/EmployeesManagementRequests.js
+++ src/api/EmployeesManagementRequests.js
@@ -32,3 +32,14 @@ export function getCurrentSchedules(jwtToken) {
 export function deleteSchedule(scheduleId, jwtToken) {
     return axios.delete(`${baseUrl}admin/employees/schedules/` + scheduleId, {headers: {'Authorization': "Bearer " + jwtToken}});
 }
+
+export function createSchedule(scheduleData, jwtToken) {
+    let body = {
+        "employeeId": scheduleData.employeeId,
+        "interval": {
+            "startDate": scheduleData.date + 'T' + scheduleData.startHour,
+            "endDate": scheduleData.date + 'T' + scheduleData.endHour
+        }
+    }
+    return axios.post(`${baseUrl}admin/employees/schedules`, body, {headers: {'Authorization': "Bearer " + jwtToken}});
+}
